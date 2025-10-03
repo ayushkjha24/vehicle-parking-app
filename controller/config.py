e5ef846fdb7e09dba97eb4a3734db576eb1b17c6
Ayush kumar jha <ayushkjha.2004@gmail.com>
2025-10-03 20:13:13 +0530

    made some changes in config

--- controller/config.py
+++ controller/config.py
@@ -1,6 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY',os.urandom(24))
-    SQLALCHEMY_DATABASE_URI = os.getenv('Database_URL','postgres://b03d6f05f12099501e8d1f6ab41b296fa462afa738f09e290ddd181389a80d36:sk_cPhmuv69mHxsb_L1qiiB4@db.prisma.io:5432/postgres?sslmode=require')
+    SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24))
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'Database_URL',
+        'postgresql://b03d6f05f12099501e8d1f6ab41b296fa462afa738f09e290ddd181389a80d36:sk_cPhmuv69mHxsb_L1qiiB4@db.prisma.io:5432/postgres?sslmode=require'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
